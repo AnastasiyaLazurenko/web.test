6a481eec44450bc963aa2c7fb30d32a9208b1c97
AnastasiyaLazurenko <66255786+AnastasiyaLazurenko@users.noreply.github.com>
2024-10-08 18:34:23 +0300

    LoginTest

---
+++ TestProject_Calc/CalculatorPage.cs
@@ -0,0 +1,9 @@
+﻿using OpenQA.Selenium.Chrome;
+
+namespace TestProject_Calc
+{
+    public class CalculatorPage(ChromeDriver driver)
+    {
+        public static string URL = "http://localhost:5000/Calculator";
+    }
+}
--- TestProject_Calc/LoginPage.cs
+++ TestProject_Calc/LoginPage.cs
@@ -4,16 +4,30 @@ using OpenQA.Selenium.Chrome;
 namespace TestProject_Calc
 {
     public class LoginPage(ChromeDriver driver)
+
     {
+        public static string URL = "http://localhost:5000/";
         private IWebElement LoginField => driver.FindElement(By.XPath("//input[@id='login']"));
         private IWebElement PasswordField => driver.FindElement(By.XPath("//input[@id='password']"));
         private IWebElement LoginButtonLocator => driver.FindElement(By.XPath("//button[text()='Login']"));
-        public IWebElement ErrorMessage => driver.FindElement(By.XPath("//th[@id='errorMessage']"));
+
+        private By ErrorLocator = By.XPath("//th[@id='errorMessage']");
 
         public void InsertLoginValue(string loginValue) => LoginField.SendKeys(loginValue);
 
         public void InsertPasswordValue(string passwordValue) => PasswordField.SendKeys(passwordValue);
 
         public void ClickLogin() => LoginButtonLocator.Click();
+
+        //public string? ErrorMessageText => driver.FindElements(ErrorLocator).FirstOrDefault()?.Text;
+
+        public string ErrorMessageText => driver.FindElement(ErrorLocator).Text;
+
+        public void EnterCredentialsAndLogin(string login, string password)
+        {
+            InsertLoginValue(login);
+            InsertPasswordValue(password);
+            ClickLogin();
+        }
     }
 }
