1cfa9f2ee3dcf49414fd27f262c76d23e37ff458
Sergii Dmytrenko <dmytrenko.sergii@gmail.com>
2024-11-09 11:49:34 +0200

    #5 Make `Calculate` button enabled when year is selected

--- WebSite/wwwroot/js/calculator.js
+++ WebSite/wwwroot/js/calculator.js
@@ -192,8 +192,10 @@ function SetCalculateButtonState() {
     var amount = Number(document.getElementById('amount').value);
     var days = Number(document.getElementById('term').value);
     var percent = Number(document.getElementById('percent').value);
+    var year360 = document.querySelector('#finYear360').checked;
+    var year365 = document.querySelector('#finYear365').checked;
 
-    document.getElementById('calculateBtn').disabled = (amount == 0 || days == 0 || percent == 0);
+    document.getElementById('calculateBtn').disabled = (amount == 0 || days == 0 || percent == 0 || (!year360 && !year365));
 }
 
 async function Calculate() {
